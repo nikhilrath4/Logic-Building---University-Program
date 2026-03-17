0456c3f2b4f5bf22fcf64de7456a496d2ec96288
Nikhil Rath <nikhilrath4@gmail.com>
2026-03-18 00:24:45 +0530

    done

---
+++ Practice Problems/Level 1/Beginner Programs/Problem 10.py
@@ -0,0 +1,8 @@
+#Problem 10: Area of Rectangle
+
+Length = float(input("Enter the length of rectangle: "))
+Width = float(input("Enter the Width of rectangle: "))
+
+Area = Length * Width
+
+print("The area of rectangle is ", Area)
