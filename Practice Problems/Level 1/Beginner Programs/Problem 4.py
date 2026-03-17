d6978af02d9837e0a717263c3caa8fbf6af3e997
Nikhil Rath <nikhilrath4@gmail.com>
2026-03-17 23:42:43 +0530

    done

---
+++ Practice Problems/Level 1/Beginner Programs/Problem 4.py
@@ -0,0 +1,11 @@
+#Problem 4: Sum and Average of Three Numbers
+
+First_Number = int(input("Enter the first number: "))
+Second_Number = int(input("Enter the Second number: "))
+Third_Number = int(input("Enter the third number: "))
+
+sum = First_Number + Second_Number + Third_Number
+print("The sum of all three numbers: ", sum)
+
+Average = sum/3
+print("The average of all three numbers: ", Average)
