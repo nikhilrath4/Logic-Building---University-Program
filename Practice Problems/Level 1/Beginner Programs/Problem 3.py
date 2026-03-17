0cfa140905884f0380b3c919e70f1c30e7e7bfc8
Nikhil Rath <nikhilrath4@gmail.com>
2026-03-17 23:08:45 +0530

    done

---
+++ Practice Problems/Level 1/Beginner Programs/Problem 3.py
@@ -0,0 +1,16 @@
+#Problem 3: Simple Calculator
+
+First_Number = int(input("Enter the first number: "))
+Second_Number = int(input("Enter the Second number: "))
+
+addition = First_Number + Second_Number
+substraction = First_Number - Second_Number
+multiplication = First_Number * Second_Number
+division = First_Number / Second_Number
+modulus = First_Number % Second_Number
+
+print("Addition: ", addition)
+print("Substraction: ", substraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+print("Modulus: ", modulus)
