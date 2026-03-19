50a19d6614d9d7bb55bf69a663220dff5291ca73
Nikhil Rath <nikhilrath4@gmail.com>
2026-03-19 22:28:18 +0530

    done

---
+++ Practice Problems/Level 1/Conditional Statements (I)/Problem 07.py
@@ -0,0 +1,9 @@
+#Problem 07: Pass or Fail
+
+Marks = float(input("Enter marks: "))
+
+if Marks >= 40:
+    print("The student got", Marks, "marks and passed")
+else:
+    print("The student got", Marks, "marks and failed")
+    
