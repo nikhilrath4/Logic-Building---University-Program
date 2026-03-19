7d2ba726a15dcae605e913f34d6a2dc8ee56d87c
Nikhil Rath <nikhilrath4@gmail.com>
2026-03-19 22:32:08 +0530

    done

---
+++ Practice Problems/Level 1/Conditional Statements (I)/Problem 08.py
@@ -0,0 +1,8 @@
+#Problem 08: Divisibility by 5
+
+Number = int(input("Enter the number: "))
+
+if Number % 5 == 0:
+    print(Number, "is divisible by 5")
+else:
+    print(Number, "is not divisible by 5")
